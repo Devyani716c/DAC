da37167892a82678f5713f60958cfaf854bc94a0
Devyani Chaudhari <devyanic716@gmail.com>
2026-07-24 10:00:27 +0530

    Implement hash function for key-value storage

---
+++ Hash.py
@@ -0,0 +1,15 @@
+
+def hash_function(key, table_size ):
+    hash_value = 0
+    for char in key:
+        hash_value += ord(char)  # Convert character to ASCII value
+    return hash_value % table_size
+
+# User input
+key = input("Enter a key: ")
+table_size = int(input("Enter hash table size: "))
+
+# Calculate hash value
+index = hash_function(key, table_size)
+
+print("Hash value (index):", index)
